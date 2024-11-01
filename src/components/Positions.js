fff95f4c5b884b7e8e590e2f6c7c79ad2da790bb
0007aadil <127722693+0007aadil@users.noreply.github.com>
2024-11-01 14:53:59 +0530

    Update Positions.js

--- src/components/Positions.js
+++ src/components/Positions.js
@@ -7,7 +7,7 @@ const Positions = () => {
   const [allPositions ,setAllPositions] = useState([]);
 
     useEffect(() => {
-      axios.get("http://localhost:5000/allPositions").then((res) => {
+      axios.get("https://backend-my-project.onrender.com/allPositions").then((res) => {
         console.log(res.data);
         setAllPositions(res.data);
       })
